f21de67e39e1553b25535d952d1fe01479328dbd
cesar <cesarmarmun@gmail.com>
2017-12-19 12:05:11 -0200

    adicionando zagueiro e atacante

--- src/Team.java
+++ src/Team.java
@@ -5,43 +5,26 @@ import simple_soccer_lib.PlayerCommander;
 public class Team extends AbstractTeam {
 
 	public Team(String suffix) {
-		super("Ex2" + suffix, 2);
+		super("P" + suffix, 2);
 	}
 
 	@Override
 	protected void launchPlayer(int ag, PlayerCommander commander) {
-		double targetX, targetY;
+		//double targetX, targetY;
 		
-		targetX = -53d / 2;
-		
-//		if (ag == 0) {
-//			targetY = 34.0d / 2; //posição que aparece mais baixa no monitor
-//			Defender pl = new Defender(commander, targetX, targetY);
-//			pl.start();
-//		} else if(ag == 1){
-//			targetY = -34.0d / 2;  //posição mais alta
-//			Exercise2Player pl = new Exercise2Player(commander, targetX, targetY);
-//			pl.start();
-//		}else if(ag == 2){
-//			targetY = -20.0d / 2;
-//			Defender plb = new Defender(commander, targetX, targetY);
-//			plb.start();
-//		}else if(ag == 3){
-//			targetY = 20.0d / 2;	
-//			Exercise2Player plc = new Exercise2Player(commander, targetX, targetY);
-//			plc.start();
-//		}else{
-//			targetY = 1.0d;
-//		}
-		if(ag%2 == 0)			targetY = 34.0d/2;
-		else if(ag%2 == 1)	targetY = -34.0d/2;
-		else				targetY = 0d;
+		if(ag==0){
+			Zagueiro p = new Zagueiro(commander, -25, 20);
+			p.start();
+		}else if(ag==1){
+			Zagueiro p = new Zagueiro(commander, -25, -20);
+			p.start();
+		}
 		
 		
 		
-		Player pl = new Player(commander, targetX, targetY);
-		//Defender pl = new Defender(commander, targetX, targetY);
-		pl.start();
+//		Player pl = new Player(commander, targetX, targetY);
+//		//Defender pl = new Defender(commander, targetX, targetY);
+//		pl.start();
 	}
 
 
